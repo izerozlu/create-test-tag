6ded28f4bd0b3e1af05945d52e7ad20bf75c3345
izerozlu <izerozlu@gmail.com>
2023-10-24 10:57:51 +0200

    Update tag number generating

--- create_test_tag.js
+++ create_test_tag.js
@@ -7,12 +7,12 @@ const getTypeParameter = () => {
 };
 
 const createAndPushTag = (tagName) => {
-  exec(`git tag ${tagName}`, (error, sdtout) => {
+  exec(`git tag ${tagName}`, (error) => {
     if (!error) {
       console.log(`Created the tag ${tagName}`);
       console.log('Pushing...');
 
-      exec(`git push origin ${tagName}`, (error, stdout) => {
+      exec(`git push origin ${tagName}`, (error) => {
         if (!error) {
           console.log(`Pushed the tag ${tagName}`);
         }
@@ -25,37 +25,44 @@ const findMaxTagNumber = (branchTags, type) => {
   return branchTags.reduce((maxTagNumber, tagName) => {
     let tagNumber = -1;
     const splitTagName = tagName.split('_');
+    const latestTagNumber = splitTagName.pop();
     try {
       if (splitTagName[0] === type) {
-        tagNumber = Number.parseInt(splitTagName.pop());
+        if (!Number.isNaN(Number.parseInt(latestTagNumber))) {
+          tagNumber = Number.parseInt(latestTagNumber);
+        } else {
+          tagNumber = 0;
+        }
       }
     } catch (e) {
       tagNumber = -1;
     }
 
     return !Number.isNaN(tagNumber)
-      ? Math.max(tagNumber, maxTagNumber)
-      : maxTagNumber;
+        ? Math.max(tagNumber, maxTagNumber)
+        : maxTagNumber;
   }, 0);
 };
 
 const grepTagNames = (currentBranch) => {
-  exec(`git tag | grep ${currentBranch}`, (error, sdtout) => {
+  exec(`git tag | grep ${currentBranch}`, (error, stdout) => {
     const type = getTypeParameter();
 
     if (!error) {
-      const branchTags = sdtout.split('\n').slice(0, -1);
+      const branchTags = stdout.split('\n').slice(0, -1);
       const maxTagNumber = findMaxTagNumber(branchTags, type);
 
-      createAndPushTag(`${type}_${currentBranch}_${maxTagNumber + 1}`);
+      if (Number.isInteger(maxTagNumber)) {
+        createAndPushTag(`${type}_${currentBranch}_${maxTagNumber + 1}`);
+      }
     } else {
       createAndPushTag(`${type}_${currentBranch}_1`);
     }
   });
 };
 
-exec('git branch --show-current', (error, sdtout) => {
+exec('git branch --show-current', (error, stdout) => {
   if (!error) {
-    grepTagNames(sdtout.trim());
+    grepTagNames(stdout.trim());
   }
 });
